58523192c1003220bc949b709a48886cbd298ca4
Navid Karami <navid.gsz@gmail.com>
2019-06-21 16:02:33 -0700

    initial commit-project setup

---
+++ src/main/java/com/lambdaschool/tripsplit/TripsplitApplication.java
@@ -0,0 +1,15 @@
+package com.lambdaschool.tripsplit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripsplitApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(TripsplitApplication.class, args);
+    }
+
+}
