b8928e356cd9b3b4c9a21550e5a63b22fe1ede75
Navid Karami <navid.gsz@gmail.com>
2019-06-24 11:33:34 -0700

    added service impls

--- src/main/java/com/lambdaschool/tripsplit/services/BillServiceImpl.java
+++ src/main/java/com/lambdaschool/tripsplit/services/BillServiceImpl.java
@@ -3,7 +3,9 @@ package com.lambdaschool.tripsplit.services;
 import com.lambdaschool.tripsplit.models.Bill;
 import com.lambdaschool.tripsplit.repository.BillRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,23 +23,40 @@ public class BillServiceImpl implements BillService
     }
 
     @Override
-    public Bill findBillById(long id)
+    public Bill findBillById(long id) throws EntityNotFoundException
     {
-        return null;
+        return billRepos.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(Long.toString(id)));
     }
 
     @Override
     public void delete(long id)
     {
-
+        if (billRepos.findById(id).isPresent())
+        {
+            billRepos.deleteById(id);
+        }
+        else
+        {
+            throw new EntityNotFoundException(Long.toString(id));
+        }
     }
 
+    @Transactional
     @Override
     public Bill save(Bill bill)
     {
-        return null;
+        Bill newBill = new Bill();
+
+        newBill.setBillTitle(bill.getBillTitle());
+        newBill.setBillAmount(bill.getBillAmount());
+        newBill.setPaidby(bill.getPaidby());
+        newBill.setTrip(bill.getTrip());
+
+        return billRepos.save(newBill);
     }
 
+    @Transactional
     @Override
     public Bill update(Bill bill, long id)
     {
--- src/main/java/com/lambdaschool/tripsplit/services/TripServiceImpl.java
+++ src/main/java/com/lambdaschool/tripsplit/services/TripServiceImpl.java
@@ -3,7 +3,9 @@ package com.lambdaschool.tripsplit.services;
 import com.lambdaschool.tripsplit.models.Trip;
 import com.lambdaschool.tripsplit.repository.TripRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,26 +23,64 @@ public class TripServiceImpl implements TripService
     }
 
     @Override
-    public Trip findByTripId(long id)
+    public Trip findByTripId(long id) throws EntityNotFoundException
     {
-        return null;
+        return tripRepos.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(Long.toString(id)));
     }
 
     @Override
     public void delete(long id)
     {
-
+        if (tripRepos.findById(id).isPresent())
+        {
+            tripRepos.deleteById(id);
+        }
+        else
+        {
+            throw new EntityNotFoundException(Long.toString(id));
+        }
     }
 
+    @Transactional
     @Override
     public Trip save(Trip trip)
     {
-        return null;
+       Trip newTrip = new Trip();
+
+       newTrip.setTripname(trip.getTripname());
+       newTrip.setStartDate(trip.getStartDate());
+       newTrip.setEndDate(trip.getEndDate());
+       newTrip.setCreatedby(trip.getCreatedby());
+
+       return  tripRepos.save(newTrip);
     }
 
+    @Transactional
     @Override
     public Trip update(Trip trip, long id)
     {
-        return null;
+        Trip currentTrip = tripRepos.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(Long.toString(id)));
+
+        if(trip.getTripname() != null)
+        {
+            currentTrip.setTripname(trip.getTripname());
+        }
+
+        if(trip.getStartDate() != null)
+        {
+            currentTrip.setStartDate(trip.getStartDate());
+        }
+
+        if(trip.getEndDate() != null)
+        {
+            currentTrip.setEndDate(trip.getEndDate());
+        }
+
+        if(trip.getCreatedby() != )
+        {
+            currentTrip.setCreatedby(trip.getCreatedby());
+        }
     }
 }
